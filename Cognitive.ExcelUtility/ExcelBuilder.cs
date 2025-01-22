4818abf427e844ad2ff4d24312e909d67ab68f83
Alex Khurtin <alexei.hurtin@gmail.com>
2025-01-22 17:36:22 +0300

    [KOGN-16] Разработка человекочитаемой выгрузки | готово

--- Cognitive.ExcelUtility/ExcelBuilder.cs
+++ Cognitive.ExcelUtility/ExcelBuilder.cs
@@ -1,5 +1,6 @@
 ﻿using Cognitive.ExcelUtility.DalPg.Entities;
 using Cognitive.ExcelUtility.DalPg.Managers;
+using NPOI.HSSF.Util;
 using NPOI.SS.UserModel;
 using NPOI.SS.Util;
 using NPOI.XSSF.UserModel;
@@ -8,16 +9,22 @@ namespace Cognitive.ExcelUtility
 {
     internal static class ExcelBuilder
     {
-        private static XSSFWorkbook _workbook = new XSSFWorkbook();
-        private static ISheet _sheet = _workbook.CreateSheet("Report");
+        private static readonly XSSFWorkbook _workbook = new XSSFWorkbook();
+        private readonly static ISheet _sheet = _workbook.CreateSheet("Report");
 
         #region User Readable Report
 
         public static async Task<IWorkbook> CreateUserReadableReport()
         {
+            const int HeaderRows = 2;
+            const int TestInfoCols = 5;
+            const int UserInfoCols = 1;
+
             var headerNames = await GetHeaderNames();
+            int overallCols = (headerNames.Count * TestInfoCols) + UserInfoCols;
+            
             IRow[] headerRows = { _sheet.CreateRow(0), _sheet.CreateRow(1) };
-            _sheet.CreateFreezePane(1, 2);
+            _sheet.CreateFreezePane(UserInfoCols, HeaderRows);
 
             // Сборка шапки юзеров
             headerRows[0].CreateAndWriteCell(0, "ИД пользователя", CellStyleHeader, 15);
@@ -26,10 +33,10 @@ namespace Cognitive.ExcelUtility
             // Сборка шапки тестов
             for (int i = 0; i < headerNames.Count; i++)
             {
-                var startRegionCol = 1 + i * 5; // 1 == количество столбцов с инфой о юзерах
+                var startRegionCol = UserInfoCols + i * TestInfoCols;
                 headerNames[i].StartRegionCol = startRegionCol;
                 headerRows[0].CreateAndWriteCell(startRegionCol, headerNames[i].TestName, CellStyleHeader);
-                _sheet.AddMergedRegion(new CellRangeAddress(0, 0, startRegionCol, startRegionCol + 4));
+                _sheet.AddMergedRegion(new CellRangeAddress(0, 0, startRegionCol, startRegionCol + TestInfoCols - 1));
 
                 headerRows[1].CreateAndWriteCell(startRegionCol + 0, "#", CellStyleHeader, 3);
                 headerRows[1].CreateAndWriteCell(startRegionCol + 1, "% выполнения", CellStyleHeader, 12);
@@ -51,7 +58,7 @@ namespace Cognitive.ExcelUtility
                     rowsCreated++;
                     if (rowsCreated == 1)
                     {
-                        row.CreateAndWriteCell(0, user.Key, CellStyleHeader);
+                        row.CreateAndWriteCell(0, user.Key, CellStyleData);
                     }
 
                     foreach (var testRes in testTry)
@@ -62,6 +69,26 @@ namespace Cognitive.ExcelUtility
                 {
                     _sheet.AddMergedRegion(new CellRangeAddress(rownum - rowsCreated, rownum - 1, 0, 0));
                 }
+
+                // Рисуем горизонтальную границу между людьми
+                for (var i = 0; i < overallCols; i++)
+                {
+                    var row = _sheet.GetRow(rownum - 1);
+                    var cell = row.GetOrCreateCell(i);
+                    cell.DrawBorder(false);
+                }
+            }
+
+            // Рисуем вертикальные границы
+            for (var i = 0; i < rownum; i++)
+            {
+                var row = _sheet.GetRow(i);
+
+                for (var j = UserInfoCols - 1; j < overallCols; j += TestInfoCols)
+                {
+                    var cell = row.GetOrCreateCell(j);
+                    cell.DrawBorder(true);
+                }
             }
 
             return _workbook;
@@ -69,11 +96,11 @@ namespace Cognitive.ExcelUtility
 
         private static void WriteTestResult(IRow row, int startCol, TestResultEntity item)
         {
-            row.CreateAndWriteCell(startCol + 0, item.TryNumber);
-            row.CreateAndWriteCell(startCol + 1, item.Accuracy);
-            row.CreateAndWriteCell(startCol + 2, item.CompleteTime);
-            row.CreateAndWriteCell(startCol + 3, item.NumberCorrectAnswers);
-            row.CreateAndWriteCell(startCol + 4, item.NumberAllAnswers);
+            row.CreateAndWriteCell(startCol + 0, item.TryNumber, CellStyleData);
+            row.CreateAndWriteCell(startCol + 1, item.Accuracy, CellStyleData);
+            row.CreateAndWriteCell(startCol + 2, item.CompleteTime, CellStyleData);
+            row.CreateAndWriteCell(startCol + 3, item.NumberCorrectAnswers, CellStyleData);
+            row.CreateAndWriteCell(startCol + 4, item.NumberAllAnswers, CellStyleData);
         }
 
         #endregion
@@ -105,6 +132,7 @@ namespace Cognitive.ExcelUtility
         #region Styles
 
         private static ICellStyle CellStyleHeader = CellStyleHeaderBuild();
+        private static ICellStyle CellStyleData = CellStyleDataBuild();
 
         private static ICellStyle CellStyleHeaderBuild()
         {
@@ -113,6 +141,16 @@ namespace Cognitive.ExcelUtility
             style.Alignment = HorizontalAlignment.Center;
             style.VerticalAlignment = VerticalAlignment.Center;
             style.WrapText = true;
+
+            return style;
+        }
+
+        private static ICellStyle CellStyleDataBuild()
+        {
+            var style = _workbook.CreateCellStyle();
+
+            style.Alignment = HorizontalAlignment.Center;
+            style.VerticalAlignment = VerticalAlignment.Center;
 
             return style;
         }
@@ -145,9 +183,8 @@ namespace Cognitive.ExcelUtility
         /// <param name="colWidth">Ширина столбца</param>
         private static void CreateAndWriteCell(this IRow row, int colnum, object? val, ICellStyle? style = null, int? colWidth = null)
         {
-            if (val == null) return;
-
             var cell = row.CreateCell(colnum);
+            if (val == null) return;
 
             if (val is long || val is int || val is decimal || val is short)
             {
@@ -167,6 +204,27 @@ namespace Cognitive.ExcelUtility
 
             if (colWidth != null) _sheet.SetColumnWidth(colnum, colWidth.Value * 256);
             if (style != null) cell.CellStyle = style;
+        }
+
+        private static void DrawBorder(this ICell cell, bool right)
+        {
+            var newstyle = _workbook.CreateCellStyle();
+            newstyle.CloneStyleFrom(cell.CellStyle);
+            
+            if (right) 
+                newstyle.BorderRight = BorderStyle.Thin;
+            else 
+                newstyle.BorderBottom = BorderStyle.Thin;
+
+            cell.CellStyle = newstyle;
+        }
+
+        private static ICell GetOrCreateCell(this IRow row, int idx)
+        {
+            var cell = row.GetCell(idx);
+            if (cell == null)
+                cell = row.CreateCell(idx);
+            return cell;
         }
 
         #endregion
--- Cognitive.ExcelUtility/Program.cs
+++ Cognitive.ExcelUtility/Program.cs
@@ -1,17 +1,21 @@
-﻿using NPOI.SS.UserModel;
-
-namespace Cognitive.ExcelUtility
+﻿namespace Cognitive.ExcelUtility
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please wait while the report is generated...");
             var workbook = ExcelBuilder.CreateUserReadableReport().Result;
-            using (FileStream fileStream = new FileStream("C:\\Users\\x1larus\\Desktop\\file.xlsx", FileMode.Create))
+            var path = $"{Directory.GetCurrentDirectory()}\\reports";
+            Directory.CreateDirectory(path);
+            var filename = $@"report_{DateTime.Now.ToString("yy-MM-dd_HH-mm-ss")}.xlsx";
+            using (FileStream fileStream = new FileStream($"{path}\\{filename}", FileMode.Create))
             {
                 workbook.Write(fileStream);
             }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"The report is generated in {path}\\{filename})");
+            Console.ReadKey();
         }
     }
 }
